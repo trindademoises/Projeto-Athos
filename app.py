d01baa8a176f7aa54b97aa001a548049bbea473f
trindademoises <trindade.negocio@gmail.com>
2026-02-20 18:57:05 -0300

    app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ if prompt := st.chat_input("Diga algo para o Athos..."):
 
     with st.chat_message("assistant"):
         response = client.chat.completions.create(
-            model="llama3-8b-8192",
+            model="llama-3.3-70b-versatile",
             messages=st.session_state.messages,
             temperature=0.7
         )
